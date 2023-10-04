77baf8ad31991f6bb355e564c887c9f1a0c7e695
Michal Havlíček <michalhav7@seznam.cz>
2023-10-04 15:19:06 +0200

    tweak test

--- tests/input/01_variable_definition.swift
+++ tests/input/01_variable_definition.swift
@@ -1,11 +1,13 @@
 var int : Int = 32
-var int2 : Int = 12e3
-var int3 : Int = 12E3
-var int4 : Int? = nil
-var double  : Double = 23.1
-var double2 : Double = 23.1e3
-var double2 : Double = 23.1E3
-var double3 : Double = 23.1e1
-var baz : String = "Hello world"
-var bar : String? = baz
-let boo : Int = nil
+//var int2 : Int = 12e3
+//var int3 : Int = 12E3
+//var int4 : Int? = nil
+//var double  : Double = 23.1
+//var double2 : Double = 23.1e3
+//var double2 : Double = 23.1E3
+//var double3 : Double = 23.1e1
+//var baz : String = "Hello world"
+//var bar : String? = baz
+//let boo : Int = nil
+
+write(int)
--- tests/test.sh
+++ tests/test.sh
@@ -2,25 +2,28 @@
 
 testNum=1
 
-# 3 arguments:
+# arguments:
 # 1. name of test
-# 2. tested function params
-# 3. correct output
+# 2. input file
+# 3. expected output file
+# 4. expected return code
 execTest () {
 	echo -e "\e[33m--------------------------------\e[0m"
 	bash -c "../bin/compiler < $2 > tmp_output.txt 2>&1"
+	ic23int tmp_output.txt > tmp_output2.txt
+	printf "\n" >> tmp_output2.txt
 	returnCode=$?
 	if [ $returnCode -ne $4 ]; then
 		printf "\e[1m\e[31mFailed\e[0m Test %02d: $1:\n" $testNum
 		printf "\tWrong return code, expected $4, got $returnCode"
-	elif cmp tmp_output.txt $3 >/dev/null 2>&1; then
+	elif [ -z "$(diff --ignore-trailing-space --ignore-blank-lines tmp_output2.txt $3)" ]; then
 		printf "\e[1m\e[32mPassed\e[0m Test %02d: $1\n" $testNum
 	else
 		printf "\e[1m\e[31mFailed\e[0m Test %02d: $1\n" $testNum
-		diff tmp_output.txt $3 | colordiff
+		diff tmp_output2.txt $3 | colordiff
 	fi
 	testNum=$((testNum+1))
-	rm -f tmp_output.txt
+	rm -f tmp_output.txt tmp_output2.txt
 }
 
-execTest "Base test" "input/00_empty.swift" output/00_empty.txt 0
+execTest "Empty program" "input/00_empty.swift" output/00_empty.txt 0
