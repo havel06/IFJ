f43ffbeeabdc142886f7cf14adb30a1084aa84b5
Michal Havlíček <michalhav7@seznam.cz>
2023-10-16 09:41:37 +0200

    more function tests

--- tests/test.sh
+++ tests/test.sh
@@ -45,9 +45,16 @@ execTest "Variable out of scope" "input/variable_out_of_scope.swift" "output/emp
 execTest "Undefined variable" "input/undefined_variable.swift" "output/empty.txt" 5
 execTest "Default nil initialisation" "input/default_nil_init.swift" "output/default_nil_init.txt" 0
 execTest "Uninitialised variable" "input/uninitialised_variable.swift" "output/empty.txt" 5
-execTest "Uninitialised variable (initialised in deeper scope)" "input/uninitialised_variable_in_scope.swift" "output/empty.txt" 5
+execTest "Uninitialised variable (init in deeper scope)" "input/uninitialised_variable_in_scope.swift" "output/empty.txt" 5
 execTest "Variable initialised in scope" "input/initialised_variable_in_scope.swift" "output/empty.txt" 0
 execTest "Function call" "input/function_call.swift" "output/function_call.txt" 0
 execTest "Function call before definition" "input/function_call_before_def.swift" "output/function_call.txt" 0
 execTest "Undefined function" "input/undefined_function.swift" "output/empty.txt" 3
 execTest "Function redefinition" "input/function_redefinition.swift" "output/empty.txt" 3
+execTest "Function call with arguments" "input/function_call_args.swift" "output/function_call_args.txt" 0
+execTest "Function with wrong parameter id" "input/funct_param_wrong_id.swift" "output/empty.txt" 5
+execTest "Function call with wrong parameter name" "input/func_param_wrong_name.swift" "output/empty.txt" 4
+execTest "Function call with omitted name" "input/func_param_omit_name.swift" "output/function_call_args.txt" 0
+execTest "Function call with illegaly omitted name" "input/func_param_omit_name_illegal.swift" "output/empty.txt" 4
+execTest "Function call with wrong parameter type" "input/func_call_wrong_type.swift" "output/empty.txt" 4
+execTest "Function call with wrong number of params" "input/func_call_wrong_param_count.swift" "output/empty.txt" 4
