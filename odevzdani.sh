c5abceefdd1b6c1e49ff40a96cd38bf796f2b0dc
Adam Krška <adam@krska.com>
2023-12-05 18:27:46 +0100

    odevzdání dokumentace

--- odevzdani.sh
+++ odevzdani.sh
@@ -3,7 +3,8 @@
 mkdir -p /tmp/ifj_odevzdani
 cp -r src/. /tmp/ifj_odevzdani/
 cp makefile_odevzdani /tmp/ifj_odevzdani/Makefile
-cp rozdeleni /tmp/ifj_odevzdani/rozdeleni
+cp rozdeleni /tmp/ifj_odevzdani/
+cp docs/out/dokumentace.pdf /tmp/ifj_odevzdani/
 cd /tmp/ifj_odevzdani && zip xhavli65.zip *
 cd -
 mv /tmp/ifj_odevzdani/xhavli65.zip xhavli65.zip
